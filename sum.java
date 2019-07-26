b44ee75ae447e48abe927341a4e70649ecbaa9d5
suryadevops79 <suryamaniprasad143@gmail.com>
2019-07-26 17:59:59 +0530

    sample java

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum {
+
+public static void main(String[] args) 
+   {
+        
+      int num1 = 5, num2 = 15, s;
+      s = num1 + num2;
+
+      System.out.println("Sum of these numbers: " + s);
+   }
+   }
