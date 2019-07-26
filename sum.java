002d78b1d4fda51ddfeb66267a6fee9b510f3da9
suryadevops79 <suryamaniprasad143@gmail.com>
2019-07-26 18:31:35 +0530

    ccc

--- sum.java
+++ sum.java
@@ -4,7 +4,7 @@ public static void main(String[] args)
    {
         
       int num1 = 5, num2 = 15, s;
-      s = num1 + num2;
+//      s = num1 + num2;
 
       System.out.println("Sum of these numbers: " + s);
    }
